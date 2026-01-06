e16cc2e129aec6a7234a2c15660c27132e88ab04
saranyarp31-svg <saranyarp31@gmail.com>
2026-01-06 14:17:53 +0530

    Create voice_output.py

---
+++ voice_output.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+import tempfile
+
+def tamil_voice(text):
+    tts = gTTS(text=text, lang="ta")
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
+    tts.save(temp.name)
+    return temp.name
